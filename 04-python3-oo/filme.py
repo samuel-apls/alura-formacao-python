83972f3900be4453397b172e7eff74d092f4baca
Samuel Damasceno <samuel.apls@gmail.com>
2021-11-22 23:41:18 -0300

    Atualização #100DaysOfCodePython R1D6

--- 04-python3-oo/filme.py
+++ 04-python3-oo/filme.py
@@ -19,15 +19,24 @@ class Programa():
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Likes: {self._likes}'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Duracao: {self.duracao} min. - Likes: {self._likes}'
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome,ano)
         self.temporadas = temporadas
+    
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Duracao: S{self.temporadas} - Likes: {self._likes}'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -39,5 +48,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+lista_filmes = [atlanta, vingadores]
+
+for programacao in lista_filmes:
+    print(programacao)
