e04e07cfd75b97c1f6253894784401d925de1ebc
Samuel Damasceno <samuel.apls@gmail.com>
2021-12-12 16:48:55 -0300

    Atualização #100DaysOfCode Python R1D14

--- 05-pyhotn-strings-parte-1/desafio_conversao.py
+++ 05-pyhotn-strings-parte-1/desafio_conversao.py
@@ -49,12 +49,26 @@ class ExtratorURL:
     def __eq__(self, comparado):
         return self.url == comparado.url
 
-url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real"
 extrator_url = ExtratorURL(url)
-extrator_url2 = ExtratorURL(url)
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+
+valor_quantidade = extrator_url.get_valor_parametro('quantidade')
+valor_origem = extrator_url.get_valor_parametro('moedaOrigem')
+valor_destino = extrator_url.get_valor_parametro('moedaDestino')
+valor_dolar = 5.50
+valor_convertido = valor_dolar * int(valor_quantidade)
 
 print(f'O tamanho da URL é: {len(extrator_url)}')
 print(extrator_url)
 print(f'Valor do parâmetro "quantidade": {valor_quantidade}')
-print(f'Os objetos são idênticos: {extrator_url == extrator_url2}')
+print(f'Valor do parâmetro "origem": {valor_origem}')
+print(f'Valor do parâmetro "destino": {valor_destino}')
+
+if valor_origem == "real" and valor_origem == "dolar":
+    valor_convertido = int(valor_quantidade) / valor_dolar
+    print(f'O valor de R${valor_quantidade} é igual a U${str(valor_convertido)}')
+elif valor_origem == "dolar" and valor_destino == "real":
+    valor_convertido = int(valor_quantidade) * valor_dolar
+    print(f'O valor de U${valor_quantidade} é igual a R${str(valor_convertido)}')
+else:
+    print(f'Câmbio de {valor_origem} para {valor_destino} não está disponível.')
